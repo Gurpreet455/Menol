fdb200c7ea2284fc68508e5ab370ff1172824158
Gurpreet Singh <117374055+Gurpreet455@users.noreply.github.com>
2024-06-23 02:35:50 +0530

    deploy

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,24 @@
 function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     const burger = document.querySelector('.burger');
+    
+    // Toggle the 'open' class on .nav-links and .burger
     navLinks.classList.toggle('open');
     burger.classList.toggle('open');
 }
+
+// Close the mobile menu when a link is clicked
+const navLinks = document.querySelectorAll('.nav-links a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        const navLinks = document.querySelector('.nav-links');
+        const burger = document.querySelector('.burger');
+        
+        // Close the menu by removing the 'open' class
+        navLinks.classList.remove('open');
+        burger.classList.remove('open');
+    });
+});
 
 
 //slider
